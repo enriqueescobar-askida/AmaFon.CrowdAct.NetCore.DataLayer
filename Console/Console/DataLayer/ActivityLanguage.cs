06e6f4ac6d1f63819f79a27915c84f8a43f97951
enriqueescobar-askida <enrique.escobar@askida.com>
2019-03-07 14:19:48 -0500

    activity language

--- Console/Console/DataLayer/ActivityLanguage.cs
+++ Console/Console/DataLayer/ActivityLanguage.cs
@@ -1,5 +1,6 @@
 ﻿namespace Console.DataLayer
 {
+    using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
 
     /// <summary>
@@ -10,20 +11,38 @@
     {
         /// <summary>
         /// Gets or sets the ActivityID
+        /// ActivityID (Primary key)
         /// </summary>
-        [Column("ActivityID")]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Column(@"ActivityID", Order = 1, TypeName = "int")] // [Column("ActivityID")]
+        // [Index(@"PK_ActivityLanguage", 1, IsUnique = true, IsClustered = true)]
+        [Required]
+        [Key]
+        [Display(Name = "Activity ID")]
         public int ActivityID { get; set; }
 
         /// <summary>
         /// Gets or sets the LanguageID
+        /// LanguageID (Primary key)
         /// </summary>
-        [Column("LanguageID")]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Column(@"LanguageID", Order = 2, TypeName = "int")] // [Column("LanguageID")]
+        // [Index(@"IX_ActivityLanguage_LanguageID", 1, IsUnique = false, IsClustered = false)]
+        // [Index(@"PK_ActivityLanguage", 2, IsUnique = true, IsClustered = true)]
+        [Required]
+        [Key]
+        [Display(Name = "Language ID")]
         public int LanguageID { get; set; }
 
         /// <summary>
         /// Gets or sets a value indicating whether Main
         /// </summary>
+        // [Column(@"Main", Order = 3, TypeName = "bit")]
+        [Required]
+        [Display(Name = "Main")]
         public bool Main { get; set; }
+
+        // Foreign keys
 
         /// <summary>
         /// Gets or sets the Activity
