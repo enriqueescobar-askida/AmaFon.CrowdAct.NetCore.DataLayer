73f0eb1d839c64429cc9bfa43e2368f5ae10dbb9
enriqueescobar-askida <enrique.escobar@askida.com>
2019-03-07 13:29:47 -0500

    user

--- Console/Console/DataLayer/User.cs
+++ Console/Console/DataLayer/User.cs
@@ -12,71 +12,118 @@
     public partial class User
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="User"/> class.
-        /// </summary>
-        public User()
-        {
-            ActivityParticipants = new HashSet<ActivityParticipant>();
-            Charities = new HashSet<Charity>();
-            ParticipantCategories = new HashSet<ParticipantCategory>();
-        }
-
-        /// <summary>
         /// Gets or sets the ID
+        /// ID (Primary key)
         /// </summary>
-        [Column("ID")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column(@"ID", Order = 1, TypeName = "int")] // [Column("ID")]
+        // [Index(@"PK_User", 1, IsUnique = true, IsClustered = true)]
+        [Required]
+        [Key]
+        [Display(Name = "Id")]
         public int ID { get; set; }
 
         /// <summary>
         /// Gets or sets the FirstName
         /// </summary>
+        // [Column(@"FirstName", Order = 2, TypeName = "nvarchar(max)")]
+        [Display(Name = "First name")]
         public string FirstName { get; set; }
 
         /// <summary>
         /// Gets or sets the LastName
         /// </summary>
+        // [Column(@"LastName", Order = 3, TypeName = "nvarchar(max)")]
+        [Display(Name = "Last name")]
         public string LastName { get; set; }
 
         /// <summary>
         /// Gets or sets the Email
         /// </summary>
-        [Required]
+        [Column(@"Email", Order = 4, TypeName = "nvarchar")]
+        // [Index(@"AK_User_Email", 1, IsUnique = true, IsClustered = false)]
+        [Required(AllowEmptyStrings = true)] // [Required]
+        [MaxLength(450)]
+        [StringLength(450)]
+        [EmailAddress]
+        [Display(Name = "Email")]
         public string Email { get; set; }
 
         /// <summary>
         /// Gets or sets the Password
         /// </summary>
+        // [Column(@"Password", Order = 5, TypeName = "nvarchar(max)")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
         public string Password { get; set; }
 
         /// <summary>
         /// Gets or sets the Phone
         /// </summary>
+        // [Column(@"Phone", Order = 6, TypeName = "nvarchar(max)")]
+        [Phone]
+        [Display(Name = "Phone")]
         public string Phone { get; set; }
 
         /// <summary>
         /// Gets or sets the BirthDate
         /// </summary>
+        // [Column(@"BirthDate", Order = 7, TypeName = "datetime2")]
+        [DataType(DataType.DateTime)]
+        [Display(Name = "Birth date")]
         public DateTime? BirthDate { get; set; }
 
         /// <summary>
         /// Gets or sets the Avatar
         /// </summary>
+        // [Column(@"Avatar", Order = 8, TypeName = "nvarchar(max)")]
+        [Display(Name = "Avatar")]
         public string Avatar { get; set; }
 
         /// <summary>
         /// Gets or sets the AddressID
         /// </summary>
-        [Column("AddressID")]
+        [Column(@"AddressID", Order = 9, TypeName = "int")] // [Column("AddressID")]
+        // [Index(@"IX_User_AddressID", 1, IsUnique = true, IsClustered = false)]
+        [Display(Name = "Address ID")]
         public int? AddressID { get; set; }
 
         /// <summary>
         /// Gets or sets the AccountStatusID
         /// </summary>
-        [Column("AccountStatusID")]
+        [Column(@"AccountStatusID", Order = 10, TypeName = "int")]
+        // [Index(@"IX_User_AccountStatusID", 1, IsUnique = false, IsClustered = false)]
+        [Column("AccountStatusID")] // [Display(Name = "Account status ID")]
         public int? AccountStatusID { get; set; }
+
+        // Reverse navigation
+
+        /// <summary>
+        /// Gets or sets the ActivityParticipants
+        /// Child ActivityParticipants where [ActivityParticipant].[ParticipantID] point to this entity (FK_ActivityParticipant_User_ParticipantID)
+        /// </summary>
+        [InverseProperty("Participant")]
+        public virtual ICollection<ActivityParticipant> ActivityParticipants { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ParticipantCategories
+        /// Child Categories (Many-to-Many) mapped by table [ParticipantCategory]
+        /// </summary>
+        [InverseProperty("Participant")]
+        public virtual ICollection<ParticipantCategory> ParticipantCategories { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Charities
+        /// Child Charities where [Charity].[RepresentativeID] point to this entity (FK_Charity_User_RepresentativeID)
+        /// </summary>
+        [InverseProperty("Representative")]
+        public virtual ICollection<Charity> Charities { get; set; }
+
+        // Foreign keys
 
         /// <summary>
         /// Gets or sets the AccountStatus
+        /// Parent AccountStatus pointed by [User].([AccountStatusId]) (FK_User_AccountStatus_AccountStatusID)
         /// </summary>
         [ForeignKey("AccountStatusId")]
         [InverseProperty("Users")]
@@ -84,27 +131,20 @@
 
         /// <summary>
         /// Gets or sets the Address
+        /// Parent Address pointed by [User].([AddressId]) (FK_User_Address_AddressID)
         /// </summary>
         [ForeignKey("AddressId")]
         [InverseProperty("User")]
         public virtual Address Address { get; set; }
 
         /// <summary>
-        /// Gets or sets the ActivityParticipants
+        /// Initializes a new instance of the <see cref="User"/> class.
         /// </summary>
-        [InverseProperty("Participant")]
-        public virtual ICollection<ActivityParticipant> ActivityParticipants { get; set; }
-
-        /// <summary>
-        /// Gets or sets the Charities
-        /// </summary>
-        [InverseProperty("Representative")]
-        public virtual ICollection<Charity> Charities { get; set; }
-
-        /// <summary>
-        /// Gets or sets the ParticipantCategories
-        /// </summary>
-        [InverseProperty("Participant")]
-        public virtual ICollection<ParticipantCategory> ParticipantCategories { get; set; }
+        public User()
+        {
+            this.ActivityParticipants = new HashSet<ActivityParticipant>();
+            this.Charities = new HashSet<Charity>();
+            this.ParticipantCategories = new HashSet<ParticipantCategory>();
+        }
     }
 }
