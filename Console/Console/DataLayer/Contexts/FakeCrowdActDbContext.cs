2837b4f119e10ec0913657379251e278fe23318d
enriqueescobar-askida <enrique.escobar@askida.com>
2019-03-08 17:12:29 -0500

    update

---
+++ Console/Console/DataLayer/Contexts/FakeCrowdActDbContext.cs
@@ -0,0 +1,262 @@
+﻿namespace Console.DataLayer.Contexts
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.ChangeTracking;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Entities;
+    using Interfaces;
+
+    /// <summary>
+    /// Defines the <see cref="FakeCrowdActDbContext" />
+    /// </summary>
+    public class FakeCrowdActDbContext : ICrowdActDbContext
+    {
+        /// <summary>
+        /// Gets or sets the AccountStatuses
+        /// </summary>
+        public virtual DbSet<AccountStatus> AccountStatuses { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Activities
+        /// </summary>
+        public virtual DbSet<Activity> Activities { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ActivityLanguages
+        /// </summary>
+        public virtual DbSet<ActivityLanguage> ActivityLanguages { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ActivityParticipants
+        /// </summary>
+        public virtual DbSet<ActivityParticipant> ActivityParticipants { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ActivitySkills
+        /// </summary>
+        public virtual DbSet<ActivitySkill> ActivitySkills { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ActivityTypes
+        /// </summary>
+        public virtual DbSet<ActivityType> ActivityTypes { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Addresses
+        /// </summary>
+        public virtual DbSet<Address> Addresses { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Categories
+        /// </summary>
+        public virtual DbSet<Category> Categories { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Charities
+        /// </summary>
+        public virtual DbSet<Charity> Charities { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Cities
+        /// </summary>
+        public virtual DbSet<City> Cities { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Countries
+        /// </summary>
+        public virtual DbSet<Country> Countries { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Languages
+        /// </summary>
+        public virtual DbSet<Language> Languages { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ParticipantCategories
+        /// </summary>
+        public virtual DbSet<ParticipantCategory> ParticipantCategories { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ParticipantStatuses
+        /// </summary>
+        public virtual DbSet<ParticipantStatus> ParticipantStatuses { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Requirements
+        /// </summary>
+        public virtual DbSet<Requirement> Requirements { get; set; }
+
+        /// <summary>
+        /// Gets or sets the RequirementStatuses
+        /// </summary>
+        public virtual DbSet<RequirementStatus> RequirementStatuses { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ResourceTypes
+        /// </summary>
+        public virtual DbSet<ResourceType> ResourceTypes { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Skills
+        /// </summary>
+        public virtual DbSet<Skill> Skills { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Users
+        /// </summary>
+        public virtual DbSet<User> Users { get; set; }
+
+        public void Dispose()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int SaveChanges()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private ChangeTracker _changeTracker;
+
+        public ChangeTracker ChangeTracker => this.ChangeTracker;
+
+        private SqlServerDatabaseFacadeExtensions _database;
+
+        public FakeCrowdActDbContext()
+        {
+            this._changeTracker = null;
+            this._database = null;
+
+            this.AccountStatuses = new FakeDbSet<AccountStatus>("Id");
+            this.Activities = new FakeDbSet<Activity>("Id");
+            this.ActivityLanguages = new FakeDbSet<ActivityLanguage>("ActivityId", "LanguageId");
+            this.ActivityParticipants = new FakeDbSet<ActivityParticipant>("ActivityId", "ParticipantId");
+            this.ActivitySkills = new FakeDbSet<ActivitySkill>("Id");
+            this.ActivityTypes = new FakeDbSet<ActivityType>("Id");
+            this.Addresses = new FakeDbSet<Address>("Id");
+            this.Categories = new FakeDbSet<Category>("Id");
+            this.Charities = new FakeDbSet<Charity>("Id");
+            this.Cities = new FakeDbSet<City>("Id");
+            this.Countries = new FakeDbSet<Country>("Id");
+            this.Languages = new FakeDbSet<Language>("Id");
+            this.ParticipantCategories = new FakeDbSet<ParticipantCategory>("Id");
+            this.ParticipantStatuses = new FakeDbSet<ParticipantStatus>("Id");
+            this.Requirements = new FakeDbSet<Requirement>("Id");
+            this.RequirementStatuses = new FakeDbSet<RequirementStatus>("Id");
+            this.ResourceTypes = new FakeDbSet<ResourceType>("Id");
+            this.Skills = new FakeDbSet<Skill>("Id");
+            this.Users = new FakeDbSet<User>("Id");
+        }
+
+        /// <summary>
+        /// Gets the SaveChangesCount
+        /// </summary>
+        public int SaveChangesCount { get; private set; }
+
+        /// <summary>
+        /// The SaveChanges
+        /// </summary>
+        /// <returns>The <see cref="int"/></returns>
+        public int SaveChanges()
+        {
+            ++SaveChangesCount;
+            return 1;
+        }
+
+        /// <summary>
+        /// The SaveChangesAsync
+        /// </summary>
+        /// <returns>The <see cref="System.Threading.Tasks.Task{int}"/></returns>
+        public Task<int> SaveChangesAsync()
+        {
+            ++SaveChangesCount;
+            return Task<int>.Factory.StartNew(() => 1);
+        }
+
+        /// <summary>
+        /// The SaveChangesAsync
+        /// </summary>
+        /// <param name="cancellationToken">The cancellationToken<see cref="System.Threading.CancellationToken"/></param>
+        /// <returns>The <see cref="System.Threading.Tasks.Task{int}"/></returns>
+        public Task<int> SaveChangesAsync(System.Threading.CancellationToken cancellationToken)
+        {
+            ++SaveChangesCount;
+            return Task<int>.Factory.StartNew(() => 1, cancellationToken);
+        }
+
+        /// <summary>
+        /// The Dispose
+        /// </summary>
+        /// <param name="disposing">The disposing<see cref="bool"/></param>
+        protected virtual void Dispose(bool disposing)
+        {
+        }
+
+        /// <summary>
+        /// The Dispose
+        /// </summary>
+        public void Dispose()
+        {
+            this.Dispose(true);
+        }
+
+        public SqlServerDatabaseFacadeExtensions Database => this._database;
+
+        /// <summary>
+        /// The Entry
+        /// </summary>
+        /// <typeparam name="TEntity"></typeparam>
+        /// <param name="entity">The entity<see cref="TEntity"/></param>
+        /// <returns>The <see cref="System.Data.Entity.Infrastructure.DbEntityEntry{TEntity}"/></returns>
+        public Entity.Infrastructure.DbEntityEntry<TEntity> Entry<TEntity>(TEntity entity) where TEntity : class
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        /// The Entry
+        /// </summary>
+        /// <param name="entity">The entity<see cref="object"/></param>
+        /// <returns>The <see cref="System.Data.Entity.Infrastructure.DbEntityEntry"/></returns>
+        public System.Data.Entity.Infrastructure.DbEntityEntry Entry(object entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        /// The GetValidationErrors
+        /// </summary>
+        /// <returns>The <see cref="System.Collections.Generic.IEnumerable{System.Data.Entity.Validation.DbEntityValidationResult}"/></returns>
+        public IEnumerable<System.Data.Entity.Validation.DbEntityValidationResult> GetValidationErrors() => throw new System.NotImplementedException();
+
+        /// <summary>
+        /// The Set
+        /// </summary>
+        /// <param name="entityType">The entityType<see cref="System.Type"/></param>
+        /// <returns>The <see cref="System.Data.Entity.DbSet"/></returns>
+        public System.Data.Entity.DbSet Set(System.Type entityType)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        /// The Set
+        /// </summary>
+        /// <typeparam name="TEntity"></typeparam>
+        /// <returns>The <see cref="System.Data.Entity.DbSet{TEntity}"/></returns>
+        public System.Data.Entity.DbSet<TEntity> Set<TEntity>() where TEntity : class
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        /// The ToString
+        /// </summary>
+        /// <returns>The <see cref="string"/></returns>
+        public override string ToString()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Console/Console/DataLayer/Contexts/FakeDbSet.cs
@@ -0,0 +1,216 @@
+﻿namespace Console.DataLayer.Contexts
+{
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Linq;
+    using System.Linq.Expressions;
+    using System.Reflection;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Defines the <see cref="FakeDbSet{T}" />
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class FakeDbSet<TEntity> : DbSet<TEntity>, IQueryable, IEnumerable<TEntity>, System.Data.Entity.Infrastructure.IDbAsyncEnumerable<TEntity> where TEntity : class
+    {
+        /// <summary>
+        /// Defines the _primaryKeys
+        /// </summary>
+        private readonly PropertyInfo[] _primaryKeys;
+
+        /// <summary>
+        /// Defines the _data
+        /// </summary>
+        private readonly ObservableCollection<TEntity> _data;
+
+        /// <summary>
+        /// Defines the _query
+        /// </summary>
+        private readonly IQueryable _query;
+
+        public FakeDbSet()
+        {
+            this._data = new ObservableCollection<TEntity>();
+            this._query = this._data.AsQueryable();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FakeDbSet{TEntity}"/> class.
+        /// </summary>
+        /// <param name="primaryKeys">The primaryKeys<see cref="string[]"/></param>
+        public FakeDbSet(params string[] primaryKeys)
+        {
+            this._primaryKeys = typeof(TEntity).GetProperties().Where(x => primaryKeys.Contains(x.Name)).ToArray();
+            this._data = new ObservableCollection<TEntity>();
+            this._query = _data.AsQueryable();
+        }
+
+        /// <summary>
+        /// The Find
+        /// </summary>
+        /// <param name="keyValues">The keyValues<see cref="object[]"/></param>
+        /// <returns>The <see cref="TEntity"/></returns>
+        public override TEntity Find(params object[] keyValues)
+        {
+            if (_primaryKeys == null)
+                throw new ArgumentException("No primary keys defined");
+            if (keyValues.Length != _primaryKeys.Length)
+                throw new ArgumentException("Incorrect number of keys passed to Find method");
+
+            IQueryable<TEntity> keyQuery = this.AsQueryable();
+            keyQuery = keyValues
+                .Select((t, i) => i)
+                .Aggregate(keyQuery,
+                    (current, x) =>
+                        current.Where(entity => _primaryKeys[x].GetValue(entity, null).Equals(keyValues[x])));
+
+            return keyQuery.SingleOrDefault();
+        }
+
+        /// <summary>
+        /// The FindAsync
+        /// </summary>
+        /// <param name="cancellationToken">The cancellationToken<see cref="System.Threading.CancellationToken"/></param>
+        /// <param name="keyValues">The keyValues<see cref="object[]"/></param>
+        /// <returns>The <see cref="System.Threading.Tasks.Task{TEntity}"/></returns>
+        public override Task<TEntity> FindAsync(CancellationToken cancellationToken, params object[] keyValues) => Task<TEntity>.Factory.StartNew(() => Find(keyValues), cancellationToken);
+
+        /// <summary>
+        /// The FindAsync
+        /// </summary>
+        /// <param name="keyValues">The keyValues<see cref="object[]"/></param>
+        /// <returns>The <see cref="System.Threading.Tasks.Task{TEntity}"/></returns>
+        public override Task<TEntity> FindAsync(params object[] keyValues) => Task<TEntity>.Factory.StartNew(() => Find(keyValues));
+
+        /// <summary>
+        /// The AddRange
+        /// </summary>
+        /// <param name="entities">The entities<see cref="System.Collections.Generic.IEnumerable{TEntity}"/></param>
+        /// <returns>The <see cref="System.Collections.Generic.IEnumerable{TEntity}"/></returns>
+        public override IEnumerable<TEntity> AddRange(IEnumerable<TEntity> entities)
+        {
+            if (entities == null) throw new ArgumentNullException("entities");
+
+            List<TEntity> items = entities.ToList();
+
+            foreach (TEntity entity in items)
+                this._data.Add(entity);
+
+            return items;
+        }
+
+        /// <summary>
+        /// The Add
+        /// </summary>
+        /// <param name="item">The item<see cref="TEntity"/></param>
+        /// <returns>The <see cref="TEntity"/></returns>
+        public override TEntity Add(TEntity item)
+        {
+            if (item == null) throw new ArgumentNullException("item");
+
+            this._data.Add(item);
+
+            return item;
+        }
+
+        /// <summary>
+        /// The RemoveRange
+        /// </summary>
+        /// <param name="entities">The entities<see cref="System.Collections.Generic.IEnumerable{TEntity}"/></param>
+        /// <returns>The <see cref="System.Collections.Generic.IEnumerable{TEntity}"/></returns>
+        public override IEnumerable<TEntity> RemoveRange(IEnumerable<TEntity> entities)
+        {
+            if (entities == null) throw new ArgumentNullException("entities");
+
+            List<TEntity> items = entities.ToList();
+
+            foreach (TEntity entity in items)
+                this._data.Remove(entity);
+
+            return items;
+        }
+
+        /// <summary>
+        /// The Remove
+        /// </summary>
+        /// <param name="item">The item<see cref="TEntity"/></param>
+        /// <returns>The <see cref="TEntity"/></returns>
+        public override TEntity Remove(TEntity item)
+        {
+            if (item == null) throw new ArgumentNullException("item");
+
+            this._data.Remove(item);
+
+            return item;
+        }
+
+        /// <summary>
+        /// The Attach
+        /// </summary>
+        /// <param name="item">The item<see cref="TEntity"/></param>
+        /// <returns>The <see cref="TEntity"/></returns>
+        public override TEntity Attach(TEntity item)
+        {
+            if (item == null) throw new ArgumentNullException("item");
+
+            this._data.Add(item);
+
+            return item;
+        }
+
+        /// <summary>
+        /// The Create
+        /// </summary>
+        /// <returns>The <see cref="TEntity"/></returns>
+        public override TEntity Create() => Activator.CreateInstance<TEntity>();
+
+        /// <summary>
+        /// The Create
+        /// </summary>
+        /// <typeparam name="TDerivedEntity"></typeparam>
+        /// <returns>The <see cref="TDerivedEntity"/></returns>
+        public override TDerivedEntity Create<TDerivedEntity>() => Activator.CreateInstance<TDerivedEntity>();
+
+        /// <summary>
+        /// Gets the Local
+        /// </summary>
+        public override ObservableCollection<TEntity> Local => _data;
+
+        /// <summary>
+        /// Gets the ElementType
+        /// </summary>
+        Type IQueryable.ElementType => _query.ElementType;
+
+        /// <summary>
+        /// Gets the Expression
+        /// </summary>
+        Expression IQueryable.Expression => _query.Expression;
+
+        /// <summary>
+        /// Gets the Provider
+        /// </summary>
+        IQueryProvider IQueryable.Provider => new FakeAsyncQueryProvider<TEntity>(_query.Provider);
+
+        /// <summary>
+        /// The GetEnumerator
+        /// </summary>
+        /// <returns>The <see cref="System.Collections.IEnumerator"/></returns>
+        IEnumerator IEnumerable.GetEnumerator() => _data.GetEnumerator();
+
+        /// <summary>
+        /// The GetEnumerator
+        /// </summary>
+        /// <returns>The <see cref="System.Collections.Generic.IEnumerator{TEntity}"/></returns>
+        IEnumerator<TEntity> IEnumerable<TEntity>.GetEnumerator() => _data.GetEnumerator();
+
+        /// <summary>
+        /// The GetAsyncEnumerator
+        /// </summary>
+        /// <returns>The <see cref="System.Data.Entity.Infrastructure.IDbAsyncEnumerator{TEntity}"/></returns>
+        System.Data.Entity.Infrastructure.IDbAsyncEnumerator<TEntity> System.Data.Entity.Infrastructure.IDbAsyncEnumerable<TEntity>.GetAsyncEnumerator() => new FakeAsyncEnumerator<TEntity>(_data.GetEnumerator());
+    }
+}
