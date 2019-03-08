87b9cc0fe3808213dbc393b63bd64368d00ce95e
enriqueescobar-askida <enrique.escobar@askida.com>
2019-03-08 16:21:58 -0500

    update

---
+++ Console/Console/DataLayer/Contexts/FakeDbAsyncEnumerable.cs
@@ -0,0 +1,50 @@
+﻿namespace Console.DataLayer.Contexts
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Linq.Expressions;
+
+    public class FakeDbAsyncEnumerable<T> : EnumerableQuery<T>, System.Data.Entity.Infrastructure.IDbAsyncEnumerable<T>, IQueryable<T>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FakeDbAsyncEnumerable{T}"/> class.
+        /// </summary>
+        /// <param name="enumerable">The enumerable<see cref="System.Collections.Generic.IEnumerable{T}"/></param>
+        public FakeDbAsyncEnumerable(IEnumerable<T> enumerable)
+            : base(enumerable)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FakeDbAsyncEnumerable{T}"/> class.
+        /// </summary>
+        /// <param name="expression">The expression<see cref="System.Linq.Expressions.Expression"/></param>
+        public FakeDbAsyncEnumerable(Expression expression)
+            : base(expression)
+        {
+        }
+
+        /// <summary>
+        /// The GetAsyncEnumerator
+        /// </summary>
+        /// <returns>The <see cref="System.Data.Entity.Infrastructure.IDbAsyncEnumerator{T}"/></returns>
+        public System.Data.Entity.Infrastructure.IDbAsyncEnumerator<T> GetAsyncEnumerator()
+        {
+            return new FakeDbAsyncEnumerator<T>(this.AsEnumerable().GetEnumerator());
+        }
+
+        /// <summary>
+        /// The GetAsyncEnumerator
+        /// </summary>
+        /// <returns>The <see cref="System.Data.Entity.Infrastructure.IDbAsyncEnumerator"/></returns>
+        IDbAsyncEnumerator System.Data.Entity.Infrastructure.IDbAsyncEnumerable.GetAsyncEnumerator()
+        {
+            return GetAsyncEnumerator();
+        }
+
+        /// <summary>
+        /// Gets the Provider
+        /// </summary>
+        IQueryProvider IQueryable.Provider => new FakeDbAsyncQueryProvider<T>(this);
+    }
+}
