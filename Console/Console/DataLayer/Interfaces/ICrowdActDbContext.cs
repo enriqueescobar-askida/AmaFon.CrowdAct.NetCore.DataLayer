3f4ed58972efc37db166d959f779d0d80b7b49d9
enriqueescobar-askida <enrique.escobar@askida.com>
2019-03-11 13:32:03 -0400

    update

--- Console/Console/DataLayer/Interfaces/ICrowdActDbContext.cs
+++ Console/Console/DataLayer/Interfaces/ICrowdActDbContext.cs
@@ -3,36 +3,131 @@
     using Entities;
     using Microsoft.EntityFrameworkCore;
     using System;
-    using System.CodeDom.Compiler;
-    using System.Collections.Generic;
+    /*using System.CodeDom.Compiler;
+    using System.Collections.Generic;*/
+    using System.Threading;
+    using System.Threading.Tasks;
 
-    public interface ICrowdActDbContext : System.IDisposable
+    /// <summary>
+    /// Defines the <see cref="ICrowdActDbContext" />
+    /// </summary>
+    public interface ICrowdActDbContext : IDisposable
     {
-        DbSet<AccountStatus> AccountStatuses { get; set; } // AccountStatus
-        DbSet<Activity> Activities { get; set; } // Activity
-        DbSet<ActivityLanguage> ActivityLanguages { get; set; } // ActivityLanguage
-        DbSet<ActivityParticipant> ActivityParticipants { get; set; } // ActivityParticipant
-        DbSet<ActivitySkill> ActivitySkills { get; set; } // ActivitySkill
-        DbSet<ActivityType> ActivityTypes { get; set; } // ActivityType
-        DbSet<Address> Addresses { get; set; } // Address
-        DbSet<Category> Categories { get; set; } // Category
-        DbSet<Charity> Charities { get; set; } // Charity
-        DbSet<City> Cities { get; set; } // City
-        DbSet<Country> Countries { get; set; } // Country
-        DbSet<Language> Languages { get; set; } // Language
-        DbSet<ParticipantCategory> ParticipantCategories { get; set; } // ParticipantCategory
-        DbSet<ParticipantStatus> ParticipantStatuses { get; set; } // ParticipantStatus
-        DbSet<Requirement> Requirements { get; set; } // Requirement
-        DbSet<RequirementStatus> RequirementStatuses { get; set; } // RequirementStatus
-        DbSet<ResourceType> ResourceTypes { get; set; } // ResourceType
-        DbSet<Skill> Skills { get; set; } // Skill
-        DbSet<User> Users { get; set; } // User
+        /// <summary>
+        /// Gets or sets the AccountStatuses
+        /// </summary>
+        DbSet<AccountStatus> AccountStatuses { get; set; }
 
+        /// <summary>
+        /// Gets or sets the Activities
+        /// </summary>
+        DbSet<Activity> Activities { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ActivityLanguages
+        /// </summary>
+        DbSet<ActivityLanguage> ActivityLanguages { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ActivityParticipants
+        /// </summary>
+        DbSet<ActivityParticipant> ActivityParticipants { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ActivitySkills
+        /// </summary>
+        DbSet<ActivitySkill> ActivitySkills { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ActivityTypes
+        /// </summary>
+        DbSet<ActivityType> ActivityTypes { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Addresses
+        /// </summary>
+        DbSet<Address> Addresses { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Categories
+        /// </summary>
+        DbSet<Category> Categories { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Charities
+        /// </summary>
+        DbSet<Charity> Charities { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Cities
+        /// </summary>
+        DbSet<City> Cities { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Countries
+        /// </summary>
+        DbSet<Country> Countries { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Languages
+        /// </summary>
+        DbSet<Language> Languages { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ParticipantCategories
+        /// </summary>
+        DbSet<ParticipantCategory> ParticipantCategories { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ParticipantStatuses
+        /// </summary>
+        DbSet<ParticipantStatus> ParticipantStatuses { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Requirements
+        /// </summary>
+        DbSet<Requirement> Requirements { get; set; }
+
+        /// <summary>
+        /// Gets or sets the RequirementStatuses
+        /// </summary>
+        DbSet<RequirementStatus> RequirementStatuses { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ResourceTypes
+        /// </summary>
+        DbSet<ResourceType> ResourceTypes { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Skills
+        /// </summary>
+        DbSet<Skill> Skills { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Users
+        /// </summary>
+        DbSet<User> Users { get; set; }
+
+        /// <summary>
+        /// The SaveChanges
+        /// </summary>
+        /// <returns>The <see cref="int"/></returns>
         int SaveChanges();
 
-        /*System.Threading.Tasks.Task<int> SaveChangesAsync();
-        System.Threading.Tasks.Task<int> SaveChangesAsync(System.Threading.CancellationToken cancellationToken);
-        System.Data.Entity.Infrastructure.DbChangeTracker ChangeTracker { get; }
+        /// <summary>
+        /// The SaveChangesAsync
+        /// </summary>
+        /// <returns>The <see cref="Task{int}"/></returns>
+        Task<int> SaveChangesAsync();
+
+        /// <summary>
+        /// The SaveChangesAsync
+        /// </summary>
+        /// <param name="cancellationToken">The cancellationToken<see cref="CancellationToken"/></param>
+        /// <returns>The <see cref="Task{int}"/></returns>
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+
+        /*System.Data.Entity.Infrastructure.DbChangeTracker ChangeTracker { get; }
         System.Data.Entity.Infrastructure.DbContextConfiguration Configuration { get; }
         System.Data.Entity.Database Database { get; }
         System.Data.Entity.Infrastructure.DbEntityEntry<TEntity> Entry<TEntity>(TEntity entity) where TEntity : class;
@@ -41,6 +136,10 @@
         System.Data.Entity.DbSet Set(System.Type entityType);
         System.Data.Entity.DbSet<TEntity> Set<TEntity>() where TEntity : class;*/
 
+        /// <summary>
+        /// The ToString
+        /// </summary>
+        /// <returns>The <see cref="string"/></returns>
         string ToString();
     }
 }
